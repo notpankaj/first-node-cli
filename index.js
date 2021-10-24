29b70f8d22dbf8cc65471e81728ac9790c677691
notpankaj <pankaj0409@yahoo.com>
2021-10-24 13:36:30 +0530

    First NODE CLI

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+console.log(`
+Hello myself - RAiNER,
+
+I'm a web developer based in Chandigarh, India. 
+and im also a good person.
+
+
+
+🧑 RealName : P a n k a j
+🐱‍💻 CodeName : RAiNER
+🌸 instagram : "https://www.instagram.com/imnotpankaj/"
+📑 CV : "https://notpankaj.github.io/"
+
+
+
+
+`);
